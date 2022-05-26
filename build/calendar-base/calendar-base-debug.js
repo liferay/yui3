e42dbe6cdb9d3a14a2319a90d686e18296116371
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2022-05-26 15:32:14 +0400

    AUI-3211 - build

--- build/calendar-base/calendar-base-debug.js
+++ build/calendar-base/calendar-base-debug.js
@@ -1483,7 +1483,7 @@ Y.CalendarBase = Y.extend( CalendarBase, Y.Widget, {
         * @static
         */
     HEADER_TEMPLATE: '<div class="yui3-g {calendar_hd_class}">' +
-                        '<div class="yui3-u {calendar_hd_label_class}" id="{calendar_id}_header" aria-role="heading">' +
+                        '<div class="yui3-u {calendar_hd_label_class}" id="{calendar_id}_header">' +
                             '{calheader}' +
                         '</div>' +
                     '</div>',

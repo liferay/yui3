ec0812f683c9213ba80e636ad716d50df9691626
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2022-10-11 09:38:18 +0400

    AUI-3216 - build

--- build/yui-throttle/yui-throttle-min.js
+++ build/yui-throttle/yui-throttle-min.js
@@ -1,3 +1,3 @@
-YUI.add("yui-throttle",function(o,t){
+YUI.add("yui-throttle",function(r,t){
 /*! Based on work by Simon Willison: http://gist.github.com/292562 */
-o.throttle=function(n,i){if(-1===(i=i||o.config.throttleTime||150))return function(){n.apply(this,arguments)};var r=o.Lang.now();return function(){var t=o.Lang.now();i<t-r&&(r=t,n.apply(this,arguments))}}},"@VERSION@",{requires:["yui-base"]});
+r.throttle=function(n,i){var o;return-1===(i=i||r.config.throttleTime||150)?function(){n.apply(this,arguments)}:(o=r.Lang.now(),function(){var t=r.Lang.now();i<t-o&&(o=t,n.apply(this,arguments))})}},"@VERSION@",{requires:["yui-base"]});
